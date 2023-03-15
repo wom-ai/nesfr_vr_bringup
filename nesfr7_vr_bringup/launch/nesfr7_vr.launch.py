232de16cd971a35edcdeedd8854234dbefcb096e
Jaeyoung Lee <jylee0121@gmail.com>
2023-03-15 21:04:59 +0900

    update: update nesfr7_vr_bringup
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

---
+++ nesfr7_vr_bringup/launch/nesfr7_vr.launch.py
@@ -0,0 +1,89 @@
+import os
+
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import (DeclareLaunchArgument, EmitEvent, ExecuteProcess,
+                            LogInfo, RegisterEventHandler, TimerAction)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import (Command, EnvironmentVariable, FindExecutable,
+                                LaunchConfiguration, LocalSubstitution,
+                                PythonExpression, PathJoinSubstitution)
+
+def generate_launch_description():
+    namespace = LaunchConfiguration('namespace')
+
+    hostname = os.uname().nodename.replace('-', '_')
+    namespace_launch_arg = DeclareLaunchArgument(
+            'namespace',
+            #default_value=EnvironmentVariable(name='HOSTNAME')
+            default_value=hostname
+            )
+
+    #
+    # nesfr_vr_ros2
+    #
+    nesfr_vr_launch = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('nesfr_vr_ros2'), 'launch',
+                    'nesfr_vr.launch.py'
+                    ])
+                ]),
+            launch_arguments={
+                'namespace': namespace
+                }.items()
+            )
+    #
+    # robot_state_publisher_node
+    #
+    nesfr_arm_params = PathJoinSubstitution(
+        [FindPackageShare("nesfr_arm_description"), "config", "nesfr7_arm.yaml"]
+    )
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution([FindPackageShare("nesfr_arm_description"), "urdf", "nesfr_arm.urdf.xacro"]),
+            " ",
+            "nesfr_arm_params:=",
+            nesfr_arm_params,
+            " ",
+            "prefix:=",
+            hostname + '/',
+            " ",
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        namespace=namespace,
+        output="both",
+        parameters=[robot_description],
+    )
+
+
+    nesfr7_arm_launch = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('nesfr_arm_bringup'), 'launch',
+                    'nesfr7_arm_common.launch.py'
+                    ])
+                ]),
+            launch_arguments={
+                'namespace': namespace
+                }.items()
+            )
+
+
+    return LaunchDescription([
+        namespace_launch_arg,
+        robot_state_publisher_node,
+        nesfr7_arm_launch,
+        nesfr_vr_launch,
+    ])
