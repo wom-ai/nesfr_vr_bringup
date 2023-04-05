3b359c9ba6651ddb311429d9a571d5d0e05e9c41
Jaeyoung Lee <jylee0121@gmail.com>
2023-04-05 20:23:55 +0900

    update: forced setup of CYCLONEDDS_URI with network's mac_addr
    Signed-off-by: Jaeyoung Lee <jylee0121@gmail.com>

--- nesfr4_vr_bringup/launch/nesfr4_vr.launch.py
+++ nesfr4_vr_bringup/launch/nesfr4_vr.launch.py
@@ -1,4 +1,6 @@
 import os
+import json
+import netifaces
 
 from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
@@ -14,6 +16,30 @@ from launch.substitutions import (Command, EnvironmentVariable, FindExecutable,
                                 PythonExpression, PathJoinSubstitution)
 
 import ament_index_python.packages
+
+os.environ['RCUTILS_COLORIZED_OUTPUT'] = '1'
+
+#
+# references
+#  - https://pypi.org/project/netifaces/
+#  - https://github.com/eclipse-cyclonedds/cyclonedds
+#
+def set_cyclonedds():
+    # FIXME
+    with open("/etc/nesfrvr/config/hw_config.json", "r") as json_file:
+        json_data = json.load(json_file)
+        print(json_data['network']['mac_addr'])
+
+    for iface in netifaces.interfaces():
+        ifaddresses = netifaces.ifaddresses(iface)
+        ret = ifaddresses.get(netifaces.AF_LINK, None)
+        if ret is not None and len(ret) > 0:
+            ret = ret[0].get('addr', None)
+            if ret is not None and ret == json_data['network']['mac_addr']:
+                os.environ['CYCLONEDDS_URI'] = '<CycloneDDS><Domain><General><NetworkInterfaceAddress>{}</></></></>'.format(iface)
+                print("[INFO] interface_name={} mac_addr={}".format(iface, ret))
+
+    print('[INFO] export CYCLONEDDS_URI=', os.environ['CYCLONEDDS_URI'])
 def generate_launch_description():
     namespace = LaunchConfiguration('namespace')
 
